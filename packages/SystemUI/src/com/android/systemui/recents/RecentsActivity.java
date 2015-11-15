64db8f8f3393af38378dbb9c13953cccd174f338
0xD34D <clark@scheffsblend.com>
2015-11-15 14:18:11 -0500

    Add clear all recents action to recents panel (Squashed)
    this includes:
    *Add clear all recents action to recents panel -0xD34D
    *Fix - place Clear Recents button above the nav bar -rajyengi
    *Fix up 'clear recents' UX. -maniac103
    
    PureNexus edits:
    squashed, and turn off native google clear recents

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -252,12 +252,16 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
             }
             mEmptyView.setVisibility(View.VISIBLE);
             mRecentsView.setSearchBarVisibility(View.GONE);
+            findViewById(R.id.clear_recents).setVisibility(View.GONE);
         } else {
             if (mEmptyView != null) {
                 mEmptyView.setVisibility(View.GONE);
             }
+
             boolean showSearchBar = Settings.System.getInt(getContentResolver(),
                        Settings.System.RECENTS_SHOW_SEARCH_BAR, 1) == 1;
+            findViewById(R.id.clear_recents).setVisibility(View.VISIBLE);
+
             if (mRecentsView.hasValidSearchBar()) {
                 if (showSearchBar) {
                     mRecentsView.setSearchBarVisibility(View.VISIBLE);
